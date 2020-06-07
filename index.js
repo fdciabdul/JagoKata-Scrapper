5ce9ad5d817e8f2b932a66bff22f0af8d3197d1b
Indeeeeeed <abdulmuttaqin456@gmail.com>
2020-06-07 07:12:21 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+
+request.get({
+  headers: {
+'user-agent' : 'Mozilla/5.0 (Linux; Android 8.1.0; vivo 1820) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Mobile Safari/537.36'
+},
+  url: 'https://jagokata.com/kata-bijak/acak.html',
+},function(error, response, body){
+    let $ = cheerio.load(body);
+    const author = $('a[class="auteurfbnaam"]').contents().first().text();
+    const  kata = $('q[class="fbquote"]').contents().first().text();
+    var getkata = kata +"\n </br>"+ author;
+    console.log(getKata);
+});
+
